d073a16aac0d1645574c0fb72e8cf30b3d8af917
yryogi <yogi.ghorecha@gmail.com>
2013-03-18 18:18:18 +0530

    Google analytic title related change and new BLACK template add in common directory

--- photos.php
+++ photos.php
@@ -1,24 +1,21 @@
 <?php
+
 global $var;
-
-/**
-Purpose: Code for Joomla menu
-Developer: Yogi
-Last Updated Date: 27-12-2012
-**/
-require('jmenu.php');
-/** Code for Joomla menu End **/
-
 include_once('./inc/var.php');
 include_once($var->inc_path.'base.php');
 _init();
 
+# Query to get photos Category name
+$param['album'] = db_fetch("select * from `jos_phocagallery_categories` where `id` = ".$var->get['album_id']);
+
 ?>
+
+
 
 <!DOCTYPE HTML>
 <html>
 <head>
-<title><?php echo $var->site_name.' | Photos'; ?></title>
+<title><?php echo $var->site_name.' | Photos | ' .$param['album']['title']; ?></title>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 <meta name="keywords" content="<?php echo $var->keywords; ?>" />
 <meta name="description" content="<?php echo $var->metadesc; ?>" />
