dc5b8e4a3bfe9907df5b9cac8ed9e088f67f15fb
yryogi <yogi@townwizard.com>
2014-02-06 20:12:33 +0530

    weekend slider issue resolved

--- modules/mod_feaslider/helper.php
+++ modules/mod_feaslider/helper.php
@@ -16,26 +16,60 @@ defined('_JEXEC') or die('Restricted access');
 
 //require_once (JPATH_SITE.DS.'components'.DS.'com_content'.DS.'helpers'.DS.'route.php');
 
-class modFeaEventHelper
-{
+class modFeaEventHelper{
 	
-	function getFeaturedSlider($toyear,$tomonth,$today)
-	{
-	global $current_cat;
-	$dateformat = mysql_query("SELECT date_format FROM jos_pageglobal LIMIT 1");
- 	$format = mysql_fetch_assoc($dateformat);	
-	
-	$LD = Date('d', strtotime("+30 days"));
-	$LM = Date('m', strtotime("+30 days"));
-	$LY = Date('y', strtotime("+30 days"));
-	
-	if(!isset($current_cat) || $current_cat == ''){
-		$query_featuredeve="SELECT rpt.rp_id, rpt.startrepeat,DATE_FORMAT(rpt.startrepeat,'%Y') as Eyear,DATE_FORMAT(rpt.startrepeat,'%m') as Emonth,DATE_FORMAT(rpt.startrepeat,'%d') as EDate,DATE_FORMAT(rpt.startrepeat,'".$format["date_format"]."') as Date,DATE_FORMAT(rpt.startrepeat,'%h:%i %p') as timestart,DATE_FORMAT(rpt.endrepeat,'%h:%i%p') as timeend,rpt.endrepeat,ev.ev_id,evd.evdet_id, ev.catid,cat.title as category,evd.description, loc.title, evd.location,evd.summary,cf.value FROM jos_jevents_vevent AS ev,jos_jevents_vevdetail AS evd,jos_jev_locations as loc, jos_categories AS cat,jos_jevents_repetition AS rpt,jos_jev_customfields AS cf WHERE rpt.eventid = ev.ev_id AND loc.loc_id = evd.location AND rpt.eventdetail_id = evd.evdet_id AND ev.catid = cat.id AND ev.state = 1 AND rpt.eventdetail_id = cf.evdet_id AND cf.value = 1 AND rpt.endrepeat >= '".$toyear."-".$tomonth."-".$today." 00:00:00' AND rpt.startrepeat <= '".$LY."-".$LM."-".$LD." 23:59:59' GROUP BY rpt.eventid,rpt.startrepeat ORDER BY rpt.startrepeat" ;
-	}else{
-		$query_featuredeve="SELECT rpt.rp_id, rpt.startrepeat,DATE_FORMAT(rpt.startrepeat,'%Y') as Eyear,DATE_FORMAT(rpt.startrepeat,'%m') as Emonth,DATE_FORMAT(rpt.startrepeat,'%d') as EDate,DATE_FORMAT(rpt.startrepeat,'".$format["date_format"]."') as Date,DATE_FORMAT(rpt.startrepeat,'%h:%i %p') as timestart,DATE_FORMAT(rpt.endrepeat,'%h:%i%p') as timeend,rpt.endrepeat,ev.ev_id,evd.evdet_id, ev.catid,cat.title as category,evd.description, loc.title, evd.location,evd.summary,cf.value FROM jos_jevents_vevent AS ev,jos_jevents_vevdetail AS evd,jos_jev_locations as loc, jos_categories AS cat,jos_jevents_repetition AS rpt,jos_jev_customfields AS cf WHERE rpt.eventid = ev.ev_id AND loc.loc_id = evd.location AND rpt.eventdetail_id = evd.evdet_id AND ev.catid = cat.id AND (cat.id=".$current_cat." OR cat.parent_id=".$current_cat.") AND cat.published = 1 AND cat.section = 'com_jevents'  AND ev.state = 1 AND rpt.eventdetail_id = cf.evdet_id AND cf.value = 1 AND rpt.endrepeat >= '".$toyear."-".$tomonth."-".$today." 00:00:00' AND rpt.startrepeat <= '".$LY."-".$LM."-".$LD." 23:59:59' GROUP BY rpt.eventid,rpt.startrepeat ORDER BY rpt.startrepeat" ;
-	}	
-	//echo $query_featuredeve;
-	$FeaturedSlider = mysql_query($query_featuredeve);
-	return $FeaturedSlider;
+	function getFeaturedSlider($toyear,$tomonth,$today){
+		
+		//Getmenu parameter to retrive category list
+		global $current_cat;
+		$dateformat = mysql_query("SELECT date_format FROM jos_pageglobal LIMIT 1");
+	 	$format = mysql_fetch_assoc($dateformat);	
+		
+		$params =& JComponentHelper::getParams( JEV_COM_COMPONENT );
+		 if ($params->get("relative","rel")=="strtotime"){
+			
+			$value = $params->get("strstart","");
+			$value = new JDate($value);
+			$startdate = $value->toFormat("%Y-%m-%d");
+			
+			$date = DateTime::createFromFormat("Y-m-d", $startdate);
+			$WEND_SD = $date->format("d");
+			$WEND_SM = $date->format("m");
+			$WEND_SY = $date->format("Y");
+
+			$value2 = $params->get("strend","");
+			$value2 = new JDate($value2);
+			$enddate = $value2->toFormat("%Y-%m-%d");
+			
+			$date2 = DateTime::createFromFormat("Y-m-d", $enddate);
+			$WEND_ED = $date2->format("d");
+			$WEND_EM = $date2->format("m");
+			$WEND_EY = $date2->format("Y");
+		}
+		
+		$LD = Date('d', strtotime("+30 days"));
+		$LM = Date('m', strtotime("+30 days"));
+		$LY = Date('Y', strtotime("+30 days"));
+			
+		$query_featuredeve.= "SELECT rpt.rp_id, rpt.startrepeat,DATE_FORMAT(rpt.startrepeat,'%Y') as Eyear,DATE_FORMAT(rpt.startrepeat,'%m') as Emonth,DATE_FORMAT(rpt.startrepeat,'%d') as EDate,DATE_FORMAT(rpt.startrepeat,'".$format["date_format"]."') as Date,DATE_FORMAT(rpt.startrepeat,'%h:%i %p') as timestart,DATE_FORMAT(rpt.endrepeat,'%h:%i%p') as timeend,rpt.endrepeat,ev.ev_id,evd.evdet_id, ev.catid,cat.title as category,evd.description, loc.title, evd.location,evd.summary,cf.value FROM jos_jevents_vevent AS ev,jos_jevents_vevdetail AS evd,jos_jev_locations as loc, jos_categories AS cat,jos_jevents_repetition AS rpt,jos_jev_customfields AS cf WHERE rpt.eventid = ev.ev_id AND loc.loc_id = evd.location AND rpt.eventdetail_id = evd.evdet_id AND ev.catid = cat.id ";
+			
+			
+		if($current_cat != ''){
+			$query_featuredeve.= "AND (cat.id=".$current_cat." OR cat.parent_id=".$current_cat.") ";
+		}
+		
+		$query_featuredeve.= "AND cat.published = 1 AND cat.section = 'com_jevents'  AND ev.state = 1 AND rpt.eventdetail_id = cf.evdet_id AND cf.value = 1 ";
+			
+		if(isset($startdate) != '' && isset($enddate) != ''){		
+			$query_featuredeve.= "AND rpt.endrepeat >= '".$WEND_SY."-".$WEND_SM."-".$WEND_SD." 00:00:00' AND rpt.startrepeat <= '".$WEND_EY."-".$WEND_EM."-".$WEND_ED." 23:59:59' ";
+		}else{
+			$query_featuredeve.= "AND rpt.endrepeat >= '".$toyear."-".$tomonth."-".$today." 00:00:00' AND rpt.startrepeat <= '".$LY."-".$LM."-".$LD." 23:59:59' ";
+		}
+		
+		$query_featuredeve.= "GROUP BY rpt.eventid,rpt.startrepeat ORDER BY rpt.startrepeat";
+			
+		//echo $query_featuredeve;
+		$FeaturedSlider = mysql_query($query_featuredeve);
+		return $FeaturedSlider;
 	}
 }
