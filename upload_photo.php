b482154739e812dd22c142a4ee6f45ccc3d6147a
Yogi <yogi@Aadits-MacBook-Pro.local>
2014-04-04 17:02:49 +0530

    photo upload size and confirmation message related changes

--- upload_photo.php
+++ upload_photo.php
@@ -1,36 +1,43 @@
 <?php
-global $var;
-include_once(JPATH_BASE .DS.'/inc/var.php');
-include_once(JPATH_BASE .DS.'/inc/base.php');
-include(JPATH_BASE .DS.'/formValidation.php');
-_init();
+	global $var;
+	include_once(JPATH_BASE .DS.'/inc/var.php');
+	include_once(JPATH_BASE .DS.'/inc/base.php');
+	include(JPATH_BASE .DS.'/formValidation.php');
+	_init();
 ?>
 <script type="text/javascript" language="javascript">
-  document.createElement('header');
-  document.createElement('nav');
-  document.createElement('section');
-  document.createElement('article');
-  document.createElement('aside');
-  document.createElement('footer');
+	document.createElement('header');
+	document.createElement('nav');
+	document.createElement('section');
+	document.createElement('article');
+	document.createElement('aside');
+	document.createElement('footer');
   
-  function form_validation() 
-  {
-	var filename=document.uploadForm.image.value;
-	var ext = filename.substring(filename.lastIndexOf('.') + 1);
-	
-	if(filename == ""){
-		alert ("Choose image for upload!");
-		document.uploadForm.image.focus();
-		return false;
- 	}else if(ext == "gif" || ext == "GIF" || ext == "JPEG" || ext == "jpeg" || ext == "jpg" || ext == "JPG" || ext == "PNG" || ext == "png"){
-		return true;
-	}else{
-		alert ("File Type must be GIF or JPG or PNG images only!");
-		document.uploadForm.image.value="";
-		document.uploadForm.image.focus();
-		return false;
+	  function form_validation() {
+	 	
+		var filename=document.uploadForm.image.value;
+		var ext = filename.substring(filename.lastIndexOf('.') + 1);
+		var iSize = ($("#image")[0].files[0].size / 1024); 
+		
+		if (iSize  > 2048) {
+			alert ("File size is greater than 2MB !");
+			document.uploadForm.image.focus();
+			return false; 
+		}
+		
+		if(filename == ""){
+			alert ("Choose image for upload!");
+			document.uploadForm.image.focus();
+			return false;
+	 	}else if(ext == "gif" || ext == "GIF" || ext == "JPEG" || ext == "jpeg" || ext == "jpg" || ext == "JPG" || ext == "PNG" || ext == "png"){
+			return true;
+		}else{
+			alert ("File Type must be GIF or JPG or PNG images only!");
+			document.uploadForm.image.value="";
+			document.uploadForm.image.focus();
+			return false;
+		}
 	}
-}
 </script>
 
 <?php
@@ -60,8 +67,6 @@ _init();
  
 	if(isset($var->post['formname']) && $var->post['formname'] && (datavalidation()==true) == 'upload.event.photo') {
 	
-		
-
 		$filename = $_FILES['image']['name'];
 		$ext = substr($filename,(strripos($filename,'.') + 1));
 		
@@ -73,7 +78,7 @@ _init();
 				'name'      => 'image',
 				'path'      => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
 				'savename'  => $_FILES['image']['name']
-		    ))) != false) {
+		    	))) != false) {
 				image_convert(array(
 					'path'  => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
 					'name'  => $image,
@@ -104,8 +109,7 @@ _init();
 		    }
 		
 		    //#DD#
-		    if($var->photo_uploaded==true)
-		    {
+		    if($var->photo_uploaded==true){
 				$rec = mysql_query("SELECT * FROM `jos_users` WHERE `id`=62");
 				$pageglobal=mysql_fetch_array($rec);
 				$adminEmail = $pageglobal['email'];
@@ -118,7 +122,7 @@ _init();
 				<tr><td align="left"><b>Photo</b> </td><td>: '. "http://{$_SERVER['HTTP_HOST']}/partner/{$_SESSION["partner_folder_name"]}/images/phocagallery/$image".'</td></tr>
 				<tr><td align="left"><b>Photo Caption</b> </td><td>: '.$var->post['caption'].'</td></tr>
 				<tr><td align="left"><b>Photo Description</b> </td><td>: '.$var->post['description'].'</td></tr>
-				<tr><td align="left"><b>Name</b> </td><td>: '.$var->post['username'].'</td></tr>
+				<tr><td align="left"><b>Name</b> </td><td>: '.$var->post['name'].'</td></tr>
 				<tr><td align="left"><b>Hometown</b> </td><td>: '.$var->post['location'].'</td></tr>
 				</table>';
 				$headers = 'MIME-Version: 1.0' . "\r\n";
@@ -146,7 +150,7 @@ _init();
 		echo '<div style="color:#FF0000;font-size: 14px;">'.$msg.'</div>';
 	}
 	if(isset($var->photo_uploaded) && $var->photo_uploaded){
-		echo "<h4>".JText::_('THANKYOU')."</h4><br />"; 
+		echo "<div style='color:green;font-size: 14px;'>".JText::_('THANKYOU')."</div><br />"; 
 		$_POST = "";
 	}
 	?>
