cda6a37d89cb245c01c867c8fb6030fc29dfbc3d
yogig <yogi.ghorecha@gmail.com>
2014-07-10 16:59:29 +0530

    changes related captach and calendar

--- upgrade/upgrade_form.php
+++ upgrade/upgrade_form.php
@@ -1,3 +1,30 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/configuration.php");
+
+// Set flag that this is a parent file
+define( '_JEXEC', 1 );
+define( 'DS', DIRECTORY_SEPARATOR );
+$x = realpath(dirname(__FILE__)."/../") ;
+define( 'JPATH_BASE', $x );
+require_once JPATH_BASE.DS.'includes'.DS.'defines.php';
+require_once JPATH_BASE.DS.'includes'.DS.'framework.php';
+$mainframe =& JFactory::getApplication('site');
+$mainframe->initialise();
+$rec		= mysql_query("select * from `jos_pageglobal`");
+$pageglobal	= mysql_fetch_array($rec);
+$site_name 	= $pageglobal['site_name'];
+$email 		= $pageglobal['email'];
+
+//$pageURL_actual	 = $_SERVER["SERVER_NAME"];
+$split_url = explode('.',$_SERVER["SERVER_NAME"]);
+if($split_url[0]=="www"){
+	$guidename = $split_url[1];
+}else{
+	$guidename = $split_url[0];
+}
+
+?>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -5,22 +32,30 @@
 body {
     background: none repeat scroll 0 0 #E4E4E4;
     font-family: arial;
-
+	color: #000;
 }
 input{
 	margin: 0px;
 }
+input[type="text"],input[type="email"]{
+	border: 1px solid rgba(0,0,0,0.3);
+	box-shadow: -1px -2px 3px rgba(0,0,0,0.2);
+	color: rgba(101,100,90,1);
+	padding: 8px;
+	width: 95%;
+}
 #upgradeform table{
-background: none repeat scroll 0 0 #FFFFFF;
+	background: none repeat scroll 0 0 #FFFFFF;
     border: 1px solid #ABABAB;
     border-radius: 5px 5px 5px 5px;
     box-shadow: 1px 2px 1px #A9A9A9;
     font-family: arial;
     font-size: 15px;
     margin: auto;
-    padding: 20px;
-    width: 648px;
+    padding:2px 10px;
+    width: 396px;
 }
+
 .button {
     background-image: linear-gradient(to top, #c00c13 48%, #d72128 51%);
     border-radius: 5px 5px 5px 5px;
@@ -31,16 +66,32 @@ background: none repeat scroll 0 0 #FFFFFF;
 	text-transform: uppercase;
 }
 
-.title{color: #FF0000;
+.admin_button{
+	border: none;
+    color: #333;
+    font-weight: bold;
+    padding: 5px 8px;
+	text-transform: uppercase;
+	text-align: center;
+	width: 209px;
+	margin: auto;
+	display: block;
+}
+
+.title{
+	color: #FF0000;
     font-size: 20px;
     line-height: 50px;
     text-align: center;
     text-decoration: underline;}
 
+.thanks{
+	margin: 0 auto;
+	width:560px;
+}
 .success {
 	border: 1px solid;
-	margin: 0 auto;
-	padding: 10px 60px;
+	padding: 10px;
 	background-repeat: no-repeat;
 	background-position: 10px center;
 	text-align: center;
@@ -50,7 +101,20 @@ background: none repeat scroll 0 0 #FFFFFF;
 	font-size: 115%;
 	margin-bottom: 10px;
 }
+.required{
+	text-align: right;
+	font-size: 12px;
+	font-weight: bold;
+	padding-right: 12px;
+}
 
+input:required{
+	outline: 2px solid transparent;
+}
+
+input:invalid:focus {
+	outline: 2px solid rgba(200, 76, 76, 0.89);
+}
 </style>
 </head>
 <body>
@@ -59,68 +123,87 @@ background: none repeat scroll 0 0 #FFFFFF;
 	<img width="296" height="75" title="ttown" alt="TW LOGO" src="tw_logo_icon.png">
 </div>
 
-<div class="title">Please fill below form for upgrading package</div>
+<div class="title">Professional Upgrade</div>
 
 <?php 
  
-if(isset($_POST['submit']) && $_POST['submit']!==NULL)
+if(isset($_POST['submit']) && $_POST['submit']!=NULL)
 {
 		//=========MAIL SENT TO SUPPORT ==========//
-		$to = "darren@townwizard.com";
+		$to = "arpit@townwizard.com";
 		$subject = $_REQUEST['guidename']. "-site needs to be upgrade";
-		$message = "Below are Guide information for upgradation.<br/><br/>";
-		$message .= "<div style='padding-right:125px;float:left'>Name</div><div>:".$_REQUEST['name']."</div>";
-		$message .= "<br/><div style='padding-right:128px;float:left'>Email</div><div>:".$_REQUEST['email']."</div>";;
-		$message .= "<br/><div style='padding-right:80px;float:left'>Guide Name</div><div>:".$_REQUEST['guidename']."</div>";;
-		$message .= "<br/><div style='padding-right:64px;float:left'>Phone Number</div><div>:".$_REQUEST['phonenumber']."</div>";;
-		$message .= "<br/><div style='padding-right:7px;float:left'>Upgrade Request Date	</div><div>:".$_REQUEST['date']."<br/><br/>Thanks!</div>";
+		$message = "The following guide has requested an upgrade.<br/><br/>";
+		$message .= "<div style='float: left;width: 190px;line-height:28px;'>
+						<div>Name:</div>
+						<div>Email:</div>
+						<div>Guide Name:</div>
+						<div>Phone Number:</div>
+						<div>Package:</div>
+						<div>Upgrade Request Date:</div>
+					</div>
+					<div style='float: left;line-height:28px;'>
+						<div>".$_REQUEST['name']."</div>
+						<div>".$_REQUEST['email']."</div>
+						<div>".$_REQUEST['guidename']."</div>
+						<div>".$_REQUEST['phonenumber']."</div>
+						<div>".$_REQUEST['packages']."</div>
+						<div>".$_REQUEST['date']."</div>
+					</div>
+					<div style='clear: both;'><br/><br/>Thanks!</div>";
+		
 		$from = $_REQUEST['email'];
 		$headers = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type:text/html;charset=iso-8859-1' . "\r\n";	
-		$headers .= "From:" . $from;
+		$headers .= 'From:' . $from;
 		$mailcheck = mail($to,$subject,$message,$headers);
 		
 		//=========MAIL SENT TO PARTNER ==========//
 		$to_p = $_REQUEST['email'];
-		$subject_p = "Your Guide ".$_REQUEST['guidename']." will be upgraded soon";
-		$message_p = "Thanks for upgrading guide ".$_REQUEST['guidename'].".<br/><br/>Our team will contact you soon and your guide will be upgraded with professional version within 2 days.<br/><br/>Thanks!</div>";
+		$subject_p = "Your TownWizard Guide Upgrade";
+		$message_p = "Thanks for upgrading! Your upgrade is currently being processed. You will be contacted shortly to complete the process.<br/><br/>The TownWizard Team</div>";
 		$from_p = "Townwizard-Operations";
 		$headers_p = 'MIME-Version: 1.0' . "\r\n";
 		$headers_p .= 'Content-type:text/html;charset=iso-8859-1' . "\r\n";	
-		$headers_p .= "From:" . $from_p;
+		$headers_p .= 'From:' . $from_p;
 		$mailcheck_p = mail($to_p,$subject_p,$message_p,$headers_p);
 		
 		if($mailcheck && $mailcheck_p){
-			echo "<div class='success'>Your site will be upgraded very soon.</div>";
+			echo "<div class='thanks'><div class='success'>Thanks for upgrading! You will be contacted shortly to complete the upgrade process.<br/><br/>The TownWizard Team</div><a class='admin_button' href='/administrator'>Back to Joomla Admin</a></div>";
 		}else{
-			echo "<div>Mail is not sent due to server issue.</div>";
+			echo "<div class='thanks'>Mail is not sent due to server issue.</div>";
 		}
 	
 }
- 
 ?>
+<?php if(!isset($_POST['submit'])) { ?>
+
 <form id="upgradeform" name="upgradeform" action="" method="POST" >
 	<table cellpadding="0" cellspacing="10">
 		<tbody>
 			<tr>
-				<td><label for="name">Name:</label></td>
-				<td><input name="name" id="name" style="width: 60%;" value="" type="text" required></td>
+				<td colspan="2">
+					<div class="required">*All fields are required</div>
+				</td>
 			</tr>
 			<tr>
-				<td><label for="email">Email id:</label></td>
-				<td><input name="email" id="email" style="width: 60%;" value="" type="email" pattern="([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})" required></td>
+				<td><label for="name">Contact Name:</label></td>
+				<td><input name="name" id="name"  value="" type="text"  oninvalid="setCustomValidity('Please complete the Contact Name field.')" onchange="try{setCustomValidity('')}catch(e){}" required>*</td>
+			</tr>
+			<tr>
+				<td><label for="email">Email Address:</label></td>
+				<td><input name="email" id="email"  value="<?php echo $email ?>" type="email" pattern="([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})" oninvalid="setCustomValidity('Please complete the Email Address field.')" onchange="try{setCustomValidity('')}catch(e){}" required>*</td>
 			</tr>
 			<tr>
 				<td><label for="guidename">Guide Name:</label></td>
-				<td><input name="guidename" id="guidename" style="width: 60%;" value="" type="text" required></td>
+				<td><input name="guidename" id="guidename"  value="<?php echo $guidename; ?>" type="text" oninvalid="setCustomValidity('Please complete the Guide Name field.')" onchange="try{setCustomValidity('')}catch(e){}" required>*</td>
 			</tr>
 			<tr>
 				<td><label for="phonenumber">Phone Number:</label></td>
-				<td><input name="phonenumber" id="phonenumber" style="width: 60%;" value="" type="text" pattern="^[+]?([\d]{0,3})?[\(\.\-\s]?([\d]{3})[\)\.\-\s]*([\d]{3})[\.\-\s]?([\d]{4})$" required></td>
+				<td><input name="phonenumber" id="phonenumber" value="" type="text" pattern="^[+]?([\d]{0,3})?[\(\.\-\s]?([\d]{3})[\)\.\-\s]*([\d]{3})[\.\-\s]?([\d]{4})$" oninvalid="setCustomValidity('Please complete the Phone Number field.ex:(000)000-0000')" onchange="try{setCustomValidity('')}catch(e){}" required>*</td>
 			</tr>
 			<tr>
 				<td><label for="Packages">Packages:</label></td>
-				<td><input name="packages" id="packages" value="$1044" type="radio" checked="checked" required> $1044 (PROFESSIONAL)</td>
+				<td><input style="width: 8%" name="packages" id="packages" value="$1044" type="radio" checked="checked" required> $1044 (PROFESSIONAL)</td>
 			</tr>
 			<tr>
 				<td><input type="hidden" value="<?php echo date("F j, Y, g:i a");?>" name="date"/></td>
@@ -134,5 +217,6 @@ if(isset($_POST['submit']) && $_POST['submit']!==NULL)
 	
 	
 </form>
+<?php } ?>
 </body>
 </html>
