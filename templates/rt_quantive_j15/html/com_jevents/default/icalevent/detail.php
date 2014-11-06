1828936a3b6661721d1c7f1e8b191b59445cb309
yogig <yogi.ghorecha@gmail.com>
2014-11-06 18:16:58 +0530

    removed unwanted tempalkte folder from v3 tempaltes

--- templates/rt_quantive_j15/html/com_jevents/default/icalevent/detail.php
+++
@@ -1,5 +0,0 @@
-<?php 
-defined('_JEXEC') or die('Restricted access');
-
-
-echo $this->loadTemplate("body");
--- templates/rt_quantive_j15/html/com_jevents/icalrepeat/edit.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('_JEXEC') or die('Restricted access');
-include_once(dirname(__FILE__)."/../icalevent/".basename(__FILE__));
