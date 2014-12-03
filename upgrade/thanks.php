53ee0822887f7f7dbfc311f895b48f79e6218473
Rinkal Gandhi <rinkal1985@gmail.com>
2014-12-03 17:03:43 +0530

    Upgrade email changes after paypal payment

--- upgrade/thanks.php
+++ upgrade/thanks.php
@@ -223,7 +223,7 @@ if(isset($_GET['item_name']) && $_GET['item_name']!="")
 												<td height="50" colspan="2" >
 													<table width="550" border="0" cellspacing="0" cellpadding="0" bgcolor="#ffffff" style="border:1px double #e3e3e3;padding:0;margin:0"><tbody>
 														
-														<tr><td height="100" width="40">&nbsp;</td><td>Dear Partner,<br><br>Thank You for upgrading! Your upgrade is currently being processed.We will process your upgrade ASAP. You will shortly receive an email once your site will be upgraded. </td><td width="40">&nbsp;</td></tr>
+														<tr><td height="100" width="40">&nbsp;</td><td>Dear Partner,<br><br>Thank You for upgrading! Your upgrade is currently being processed. We will process your upgrade ASAP. You will shortly receive an email once your site will be upgraded. </td><td width="40">&nbsp;</td></tr>
 														
 														<tr><td height="100">&nbsp;</td><td> 
 							<p style="font:14px Helvetica Neue,Helvetica,Arial,sans-serif;color:#777777;margin:20px 0 5px 0;padding:0;">Thank You,</p>
