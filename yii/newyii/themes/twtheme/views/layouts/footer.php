5ce3f8f18cf91ecad1335a5a1a4bea6841dbfe54
yogig <yogi.ghorecha@gmail.com>
2015-01-19 14:00:32 +0530

    removed yii from v3 directory

--- yii/newyii/themes/twtheme/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-<hr>
-<footer class="row">
-        <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="http://www.townwizard.com" target="_blank">Townwizard</a> 2011 - <?php echo date('Y'); ?></p>
-        <p class="col-md-3 col-sm-3 col-xs-12 powered-by">Powered by: <a href="http://www.townwizard.com" target="_blank">Townwizard</a></p>
-</footer>
-</hr>
