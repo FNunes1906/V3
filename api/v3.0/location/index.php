3cddaa3580f9710b446e6915c2d7a23fa1e730f0
yryogi <yogi.ghorecha@gmail.com>
2016-06-24 19:26:15 +0530

    feature locatoin ssue solved

--- api/v3.0/location/index.php
+++ api/v3.0/location/index.php
@@ -111,7 +111,8 @@ if(isset($catId) && $catId != ''){
 	
 	# Fetch all location data for category array
 	$select_query = "	SELECT `jos_jev_locations`.*, `jos_jev_customfields3`.`value` FROM `jos_jev_locations`
-						INNER JOIN `jos_jev_customfields3` ON (`jos_jev_customfields3`.`target_id` = `jos_jev_locations`.`loc_id`) AND ";
+						INNER JOIN `jos_jev_customfields3` ON (`jos_jev_customfields3`.`target_id` = `jos_jev_locations`.`loc_id`)
+						WHERE (`jos_jev_locations`.`published` = 1) AND ";
 						
 	# Concate Category ID query to fetch location for category
 	for($k = 0; $k < count($catIdArray) ; $k++){	
@@ -126,7 +127,7 @@ if(isset($catId) && $catId != ''){
 		$select_query .= "AND `jos_jev_customfields3`.`value` = 1 ";
 	}
 	
-	$select_query .= " WHERE (`jos_jev_locations`.`published` = 1) GROUP BY `jos_jev_locations`.`loc_id` ORDER BY `jos_jev_locations`.`title` ASC";
+	$select_query .= " GROUP BY `jos_jev_locations`.`loc_id` ORDER BY `jos_jev_locations`.`title` ASC";
 	
 	# To check if Limit is given then apply in query
 	if(isset($limit) && $limit != 0){
@@ -136,7 +137,7 @@ if(isset($catId) && $catId != ''){
 			$select_query .= " limit $limit";
 	}
 	# NEW QUERY END
-/*	exit($select_query);*/
+
 	# Creating data set variable from Mysql query	
 	$result			= mysql_query($select_query);
 	//$num_records	= mysql_num_rows($result);
@@ -175,10 +176,11 @@ if(isset($catId) && $catId != ''){
 			$value['image_url'] 				= ($row['image'] != '')?$imagePath.$row['image']:NULL;
 			$value['is_featured_location'] 		= $row['value'];
 			$num_records++;
-		}
+			
 			if(isset($value) && count($value) > 0 ){
 				$data[] = $value;
 			}
+		}
 	}
 
 	# Assigning Array values to $data array variable
